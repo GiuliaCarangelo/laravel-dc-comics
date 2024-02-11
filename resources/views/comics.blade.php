3bcc22363d9d4d8c0a30902b69f2b234bd818fc0
GiuliaCarangelo <tnhkagura@gmail.com>
2024-02-11 04:55:27 +0100

    created model, migration, resource controller, index()

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/style.css">
+    <title>Comics Laravel Project</title>
+</head>
+<body>
+    <header>
+        <img src="{{ asset('img/laravel-dc-main-logo.svg') }}" alt="DC Main Logo">
+    </header>
+
+    <main>
+        <h1 class="text-center fs-1 fw-bold pt-3 pb-2">Comics Database</h1>
+        @foreach ($comics as $comic)
+        <div class="main-container">
+            <div class="section-bg">
+                <section class="row-container max-vw-100 row d-flex align-content-center justify-content-center my-5 mx-3">
+                    <div class="position-relative col-3 d-flex flex-nowrap">
+                        <div class="img-container">
+                            <img class="img-thumbnail rounded float-start d-flex" src="{{ $comic->thumb }}" alt="{{ $comic->thumb }}">
+                        </div>
+                    </div>
+                    <div class="info-container col-7 d-flex flex-column">
+                        <h2 class="fw-bold">{{ $comic->title }}</h2>
+                        <h3 class="fs-6 ">{{ $comic->description }} </h3>
+                        <h3 class="fs-2 fw-semibold d-flex ">Price: {{ $comic->price }} $</h3>
+                            <div>
+                                <div class="subtext-container d-flex flex-row mx-1">
+                                    <div class="col-6">
+                                        <div id="info-column" class="d-flex flex-column justify-between">
+                                            <h3>Genre</h3>
+                                            <h4>{{ $comic->type }}</h4>
+                                        </div>
+                                        
+                                        <div id="info-column" class="d-flex flex-column mb-3 pr-4">
+                                            <h3>Series</h3>
+                                            <h4>{{ $comic->series }}</h4>
+                                        </div>
+
+                                        <div id="info-column" class="d-flex flex-column mb-3 pr-4">
+                                            <h3>Sale Date</h3>
+                                            <h4>{{ $comic->sale_date }}</h4>
+                                        </div>
+
+                                        <div id="info-column" class="d-flex flex-column mb-3 pr-4">
+                                            <h3>Artist(s)</h3>
+                                            <h4>{{ $comic->artists }}</h4>
+                                        </div>
+
+                                        <div id="info-column" class="d-flex flex-column mb-3 pr-4">
+                                            <h3>Writer(s)</h3>
+                                            <h4>{{ $comic->writers }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        @endforeach
+    </main>
+</body>
+</html>
---
+++ resources/views/show.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Comic</title>
+</head>
+<body>
+    <section class="row my-5 mx-3">
+        <div class="col-3 d-flex flex-nowrap align-items-center">
+            <img class="img-thumbnail rounded-4" src="{{ $comic->thumb }}" alt="{{ $comic->thumb }}">
+        </div>
+        <div class="col-9 d-flex flex-column py-5">
+            <h2 class="fw-bold">{{ $comic->title }}
+            <h3 class="fs-5">{{ $comic->description }} </h3>
+            <h3 class="fs-2">Price: </h3> {{ $comic->price }} $
+            <h2>Series: </h2>{{ $comic->series }} 
+            <h2>Sale Date: </h2>{{ $comic->sale_date }}
+            <h2>Genre: </h2>{{ $comic->type }}
+            <h2>Artist(s): </h2>{{ $comic->artists }}
+            <h2>Writer(s): </h2>{{ $comic->write2s }}
+        </div>
+    </section>
+
+</body>
+</html>
